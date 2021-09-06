f0887e77c092c1470b59d4c6a49c7ca5a426d30f
MyDell <1693625254@qq.com>
2021-09-07 07:29:07 +0800

    20210907

--- pd-base/src/main/java/com/itheima/pinda/service/truck/IPdTruckTypeGoodsTypeService.java
+++ pd-base/src/main/java/com/itheima/pinda/service/truck/IPdTruckTypeGoodsTypeService.java
@@ -6,13 +6,20 @@ import com.itheima.pinda.entity.truck.PdTruckTypeGoodsType;
 import java.util.List;
 
 /**
- * @author Zhang
- * @date 9/2/2021 2:43 PM
- * @description
- *   车辆类型与货物类型关联
+ * <p>
+ * 车辆类型与货物类型关联表 服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2019-12-20
  */
-
 public interface IPdTruckTypeGoodsTypeService extends IService<PdTruckTypeGoodsType> {
+    /**
+     * 添加车辆类型与货物类型关联
+     *
+     * @param pdTruckTypeGoodsType 车辆类型与货物类型信息
+     */
+    void saveTruckTypeGoodsType(PdTruckTypeGoodsType pdTruckTypeGoodsType);
 
     /**
      * 批量添加车辆类型与货物类型关联
@@ -22,6 +29,14 @@ public interface IPdTruckTypeGoodsTypeService extends IService<PdTruckTypeGoodsT
     void batchSave(List<PdTruckTypeGoodsType> truckTypeGoodsTypeList);
 
     /**
+     * 删除关联关系
+     *
+     * @param truckTypeId 车辆类型id
+     * @param goodsTypeId 货物类型id
+     */
+    void delete(String truckTypeId, String goodsTypeId);
+
+    /**
      * 获取车辆类型与货物类型关联
      *
      * @param truckTypeId 车辆类型id
@@ -29,12 +44,4 @@ public interface IPdTruckTypeGoodsTypeService extends IService<PdTruckTypeGoodsT
      * @return 车辆类型与货物类型关联
      */
     List<PdTruckTypeGoodsType> findAll(String truckTypeId, String goodsTypeId);
-
-    /**
-     * 删除关联关系
-     *
-     * @param truckTypeId 车辆类型id
-     * @param goodsTypeId 货物类型id
-     */
-    void delete(String truckTypeId, String goodsTypeId);
 }
