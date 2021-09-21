67b7c4e0b3cbab332a42c2cfb8b1ce838a70ded3
MyDell <1693625254@qq.com>
2021-09-21 16:12:48 +0800

    20210921

--- pd-oms/src/test/java/com/itheima/test/BaiduMapTest.java
+++ pd-oms/src/test/java/com/itheima/test/BaiduMapTest.java
@@ -1,6 +1,8 @@
 package com.itheima.test;
 
+
 import com.alibaba.fastjson.JSON;
+import lombok.val;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -14,45 +16,42 @@ import java.util.Map;
  * 测试百度地图提供的地理编码HTTP服务接口
  */
 public class BaiduMapTest {
-    public static void main(String[] args) throws Exception{
-        String ak = "UEBQm9c3KZ5LrsO2C2qsOAs1eSdLvlzM";//注意，ak类型必须为服务端类型
+    public static void main(String[] args) throws Exception {
+        String ak = "1XrViM2r3sSuREis5uHU1x1dNcESDOoD";//注意，ak类型必须为服务端类型
         String address = "北京市育新花园小区";
         String httpUrl = "http://api.map.baidu.com/geocoding/v3/?address=" + address + "&output=json&ak=" + ak;
 
         URL url = new URL(httpUrl);
         URLConnection urlConnection = url.openConnection();
-
         StringBuilder stringBuilder = new StringBuilder();
-
         InputStream inputStream = urlConnection.getInputStream();
-
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+        //封装成带有缓存功能的字符串
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
         String inputLine = null;
-        while ((inputLine = bufferedReader.readLine()) != null){
+        //若数据为空，则一直读
+        while ((inputLine = bufferedReader.readLine()) != null) {
+//            将每行inputLine数据存入stringbuilder对象
             stringBuilder.append(inputLine);
         }
-
         bufferedReader.close();
+        System.out.println(stringBuilder);
 
-        System.out.println(stringBuilder.toString());
-
-        Map map = JSON.parseObject(stringBuilder.toString(), Map.class);
-        System.out.println(map);
+        Map map = JSON.parseObject(stringBuilder.toString());
 
         String status = map.get("status").toString();
-        if(status.equals("0")){
-            //返回结果成功，能够正常解析地址信息
+        //获取成功
+        if (status.equals("0")) {
             Map result = (Map) map.get("result");
             Map location = (Map) result.get("location");
-            String lng = location.get("lng").toString();
-            String lat = location.get("lat").toString();
-
+            String lngStr = location.get("lng").toString();
+            String latStr = location.get("lat").toString();
             DecimalFormat df = new DecimalFormat("#.######");
-            String lngStr = df.format(Double.parseDouble(lng));
-            String latStr = df.format(Double.parseDouble(lat));
-            String r = lngStr + "," + latStr;
+            String lng = df.format(Double.parseDouble(lngStr));
+            String lat = df.format(Double.parseDouble(latStr));
+
+            String r = lng + "," + lat;
             System.out.println(r);
         }
-
     }
 }
+
